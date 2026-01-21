46e62fa9a43e2d397a7ffe5c57c04f9a4ef15c61
anuoluwapods <ifeoluwapobalogun1@gmail.com>
2026-01-21 22:23:03 +0100

    pipeline

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -3,12 +3,12 @@ import pandas as pd
 
 print('arguments', sys.argv)
 
-month = sys.argv[1]
+month = int(sys.argv[1])
 
-df = pd.DataFrame({"day": [1, 2], "num_passengers": [3,4]})
+df = pd.DataFrame({"day": [1, 2], "num_passengers": [3, 4]})
 df['month'] = month
 print(df.head())
 
 df.to_parquet(f"output_{month}.parquet")
 
-print(f'Hello pipeline, month={month}')
+print(f'hello pipeline, month={month}')
